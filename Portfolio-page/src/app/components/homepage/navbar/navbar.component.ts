0dbda666b912c55bc0bdf269669065a9dc272987
Borkesz Máté <borkeszmate@gmail.com>
2018-09-17 22:21:07 +0200

    Collapsed navbar edited

--- Portfolio-page/src/app/components/homepage/navbar/navbar.component.ts
+++ Portfolio-page/src/app/components/homepage/navbar/navbar.component.ts
@@ -8,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
+  isToggled = false;
+  openedNav;
   constructor() { }
 
   ngOnInit() {
+    // this.openedNav = document.querySelector("#open-navbar");
+    // this.openedNav.style.display = "none";
+  }
+
+  public navClick() {
+    this.isToggled = !this.isToggled;
+    console.log(this.isToggled);
+
+    // if (this.isToggled) {
+    //   this.openedNav.style.display = "block";
+    // } else {
+    //   this.openedNav.style.display = "none";
+    // }
   }
 
 }
