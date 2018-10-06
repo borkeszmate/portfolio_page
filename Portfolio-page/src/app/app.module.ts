115409f45ed9d2f058325a68f846047bc3abe65d
Borkesz Máté <borkeszmate@gmail.com>
2018-10-06 10:54:08 +0200

    app.modules webdev-project capitalization fixed. Now prod buildi works well

--- Portfolio-page/src/app/app.module.ts
+++ Portfolio-page/src/app/app.module.ts
@@ -9,14 +9,14 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { NavbarComponent } from './components/homepage/navbar/navbar.component';
 import { HeroComponent } from './components/homepage/hero/hero.component';
-import { ProjectsListComponent } from './components/Webdev-projects/projects-list/projects-list.component';
+import { ProjectsListComponent } from './components/webdev-projects/projects-list/projects-list.component';
 import { SplitScreenComponent } from './components/homepage/split-screen/split-screen.component';
-import { ProjectDetailsComponent } from './components/Webdev-projects/project-details/project-details.component';
+import { ProjectDetailsComponent } from './components/webdev-projects/project-details/project-details.component';
 import { WebdevProjectsComponent } from './components/webdev-projects/webdev-projects.component';
 
-import { WebDevSkillsComponent } from './components/Webdev-projects/web-dev-skills/web-dev-skills.component';
-import { WebDevHeroSectionComponent } from './components/Webdev-projects/web-dev-hero-section/web-dev-hero-section.component';
-import { SectionRowComponent } from './components/Webdev-projects/section-row/section-row.component';
+import { WebDevSkillsComponent } from './components/webdev-projects/web-dev-skills/web-dev-skills.component';
+import { WebDevHeroSectionComponent } from './components/webdev-projects/web-dev-hero-section/web-dev-hero-section.component';
+import { SectionRowComponent } from './components/webdev-projects/section-row/section-row.component';
 import { CvComponent } from './components/cv/cv.component';
 import { ExperienceComponent } from './components/cv/experience/experience.component';
 import { PersonalComponent } from './components/cv/personal/personal.component';
@@ -27,7 +27,7 @@ import { LeaseplanComponent } from './components/online-marketing/leaseplan/leas
 
 
 /* SERVICES */
-import { ProjectsService } from './components/Webdev-projects/sevices/projects-service.service';
+import { ProjectsService } from './components/webdev-projects/sevices/projects-service.service';
 import { FooterComponent } from './components/homepage/footer/footer.component';
 
 
